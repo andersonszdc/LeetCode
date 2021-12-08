2626d4751fb5af25637b8c0c0ec5e820389052e8
anderson <andersonszdc@gmail.com>
2021-12-08 06:35:14 -0300

    resolve this linked list problem

--- 21-mergeTwoLists.js
+++ 21-mergeTwoLists.js
@@ -1,9 +1,33 @@
-const list1 = [1,2,4]
-const list2 = [1,3,4]
-
-const mergeTwoLists = (list1, list2) => {
-    const arr3 = list1.concat(list2)
-    return arr3
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
 }
 
-console.log(mergeTwoLists(list1, list2))
+const L1 = new ListNode(2)
+const L2 = new ListNode(5)
+const mergeTwoLists = (l1, l2) => {
+    let list = new ListNode()
+    let head = list
+    
+    while (l1 !== null && l2 !== null) {
+
+        if (l1.val < l2.val) {
+            list.next = new ListNode(l1.val)
+            l1 = l1.next
+        } else {
+            list.next = new ListNode(l2.val)
+            l2 = l2.next
+        }
+        
+        list = list.next
+    }
+
+    if (l1 !== null)
+        list.next = l1
+    if (l2 !== null)
+        list.next = l2
+    
+    return head.next
+}
+
+console.log(mergeTwoLists(L1, L2))
