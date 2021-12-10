fdcbaf7731218bd86ebfb19c2d4cf91230719589
anderson <andersonszdc@gmail.com>
2021-12-10 05:14:57 -0300

    resolve this remove element problem

---
+++ 27-removeElement.js
@@ -0,0 +1,23 @@
+// solution 1
+
+const removeElement = function(nums, val) {
+    for (i=0; i<nums.length; i++) {
+        if (nums[i] === val) {
+            nums.splice(i, 1)
+            i--
+        }
+    }
+};
+
+// solution 2
+
+const removeElement2 = function(nums, val) {
+    let k=0;
+    nums.forEach((i) => {
+        if (i !== val) {
+            nums[k] = i;
+            k++
+        }
+    })
+    return k
+}
